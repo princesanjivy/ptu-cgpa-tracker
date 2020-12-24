42d6012e9aec53c662c2a1227abb2d49cea4f06b
PrinceSanjivy <sanjivy.android@gmail.com>
2020-12-24 13:30:40 +0530

    created files and set text theme

---
+++ android/app/src/main/java/com/princeappstudio/ptu_cgpa_tracker/MainActivity.java
@@ -0,0 +1,6 @@
+package com.princeappstudio.ptu_cgpa_tracker;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ptu_cgpa_tracker/pages/home.dart';
+
+void main() => runApp(MyRootApp());
+
+class MyRootApp extends StatefulWidget {
+  MyRootApp({Key key}) : super(key: key);
+
+  @override
+  _MyRootAppState createState() => _MyRootAppState();
+}
+
+class _MyRootAppState extends State<MyRootApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "PTU CGPA Tracker",
+      theme: ThemeData(
+        textTheme: GoogleFonts.openSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: Home(),
+    );
+  }
+}
