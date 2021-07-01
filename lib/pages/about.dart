6dc3569d1016f1be9ada022a3f6f9c5db584f718
Thamizh813 <75999346+Thamizh813@users.noreply.github.com>
2021-07-01 19:01:06 +0530

    New

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
@@ -8,50 +10,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'ABOUT',
-        home: Scaffold(
-          backgroundColor: Colors.black,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.white12,
-                      width: 10,
-                    ),
-                  ),
-                  child: Image.asset(
-                    'assets/undraw_Calculator_0evy.png',
-                  ),
+      debugShowCheckedModeBanner: false,
+      home: About(),
+    );
+  }
+}
+
+class About extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "About",
+          style: GoogleFonts.raleway(
+              textStyle: TextStyle(
+            color: Colors.deepOrange,
+            fontSize: 17,
+            fontWeight: FontWeight.bold,
+          )),
+        ),
+        backgroundColor: Colors.white12,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.white12,
+                  width: 10,
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(40, 10, 40, 20),
-                child: Text(
-                  'Allow PEC students to calculate their CGPA, keep track of current semester’s CGPA so that it will give overall CGPA and current sem’s CGPA, in addition to that, department wise CGPA rank system overall & per semester',
-                  style: GoogleFonts.raleway(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.deepOrange,
-                  ),
-                ),
+              child: Image.asset(
+                'assets/undraw_Calculator_0evy.png',
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(40, 10, 40, 20),
-                child: Text(
-                  'It is easy and quick app to generate the CGPA for all semester and current semester',
-                  style: GoogleFonts.raleway(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.deepOrange,
-                  ),
-                ),
-              )
-            ],
+            ),
           ),
-        ));
+          Padding(
+            padding: EdgeInsets.fromLTRB(40, 10, 40, 20),
+            child: Text(
+              'Allow PEC students to calculate their CGPA, keep track of current semester’s CGPA so that it will give overall CGPA and current sem’s CGPA, in addition to that, department wise CGPA rank system overall & per semester',
+              style: GoogleFonts.raleway(
+                fontSize: 15,
+                fontWeight: FontWeight.w600,
+                color: Colors.deepOrange,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(40, 10, 40, 20),
+            child: Text(
+              'It is easy and quick app to generate the CGPA for all semester and current semester',
+              style: GoogleFonts.raleway(
+                fontSize: 15,
+                fontWeight: FontWeight.w600,
+                color: Colors.deepOrange,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/pages/results.dart
+++ lib/pages/results.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Result(),
+    );
+  }
+}
+
+class Result extends StatefulWidget {
+  @override
+  _ResultState createState() => _ResultState();
+}
+
+class _ResultState extends State<Result> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Result"),
+      ),
+    );
+  }
+}
